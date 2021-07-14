50744623c3f710eb286f8bbeaee1602cfe90f4a0
Drew Davis <drewdavis@drews-mbp.myfiosgateway.com>
2021-07-14 19:03:35 -0400

    change map color

--- logic.js
+++ logic.js
@@ -51,15 +51,15 @@ function createFeatures(earthquakeData) {
     createMap(earthquakes);
 };
 
-// Declare function to create map
+// Declare function to create map (day 1, ex 1)
 function createMap(earthquakes) {
     // Get initial light layer
     var mapLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
       tileSize: 512,
-      maxZoom: 18,
+      maxZoom: 11,
       zoomOffset: -1,
-      id: "mapbox/light-v10",
+      id: "mapbox/streets-v10",
       accessToken: API_KEY
     });
     // Declare map object and set it to the map element in the DOM
